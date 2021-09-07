fedb091b65bbcf5e9dffd809763b94dd786dfd17
dxsigm <54343067+dxsigm@users.noreply.github.com>
2021-09-07 17:12:35 -0400

    Session 3-1

---
+++ Week 03/Session 1/MethodsEnumStruct/Program.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.IO;
+
+namespace MethodsEnumStruct
+{
+    enum GenderPronoun
+    {
+        him,
+        her,
+        them
+    }
+
+    enum CollegeYear : byte
+    {
+        freshman = 24,
+        sophomore = 23,
+        junior = 22,
+        senior = 21
+    }
+
+    struct StudentStruct
+    {
+        // member variables with defined accessibility
+        public CollegeYear eCollegeYear;
+        public GenderPronoun eGender;
+        public string sName;
+        public double grade;
+        private string password;
+
+        // member property for accessing the private password field
+        public string Password
+        {
+            get
+            {
+                return Decrypt(this.password);
+            }
+
+            set
+            {
+                this.password = Encrypt(value);
+            }
+        }
+
+        private string Decrypt(string pw)
+        {
+            string decryptedValue = "";
+            char[] cPassword;
+
+            this.password = "structure member field";
+            pw = "local variable";
+
+            cPassword = pw.ToCharArray();
+            foreach (char c in cPassword)
+            {
+                decryptedValue += (c + 1);
+            }
+
+            return decryptedValue;
+        }
+
+        private string Encrypt(string password)
+        {
+            string encryptedValue = "";
+            char[] cPassword;
+
+            cPassword = password.ToCharArray();
+            foreach (char c in cPassword)
+            {
+                encryptedValue += (c - 1);
+            }
+
+            return encryptedValue;
+        }
+    }
+
+    struct CourseStruct
+    {
+        public string courseCode;
+        public StudentStruct[] students;
+
+        public CourseStruct(string szCourseCode, StudentStruct[] students)
+        {
+            this.courseCode = szCourseCode;
+            this.students = students;
+        }
+
+        public void AddStudent(StudentStruct student)
+        {
+            StudentStruct[] s;
+            int newLength;
+            int i = 0;
+
+            // if the students array has not been created yet, set the new length to 1,
+            // otherwise newLength should be 1 + the current Length (to hold the new student we are adding)
+            newLength = (this.students == null) ? 1 : (this.students.Length + 1);
+
+            // create the array of the new length
+            s = new StudentStruct[newLength];
+
+            // if the students array has existing values in it
+            if (this.students != null)
+            {
+                // copy them into our new array
+                foreach (StudentStruct st in this.students)
+                {
+                    s[i] = st;
+                    ++i;
+                }
+            }
+
+            // append our new student to the new array
+            s[i] = student;
+
+            // set our students array to point to the new array
+            this.students = s;
+        }
+    }
+
+    // Class Examples
+    // Author: David Schuh
+    // Purpose: Contains examples for Week 3
+    // Restrictions: Only contains code snippets.  
+    static class Examples
+    {
+        // Method: Main
+        // Purpose: The main entry point for the executable. Code snippet examples.
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            ///////////////////////////////////////////////////////
+            // Methods
+            ///////////////////////////////////////////////////////
+            {
+                int i = 0;
+                for (i = 0; i < 10; ++i)
+                {
+                    Console.WriteLine($"{i + 34 + "!"} is " + (IsEven(i) ? "even" : "odd"));
+                }
+
+                // further example of the ternary operator
+                // set the string to "even" or "odd" depending on whether i is even or odd
+                // (boolean test) ? "value if true" : "value if false"
+                string sResult = (i % 2 == 0) ? "even" : "odd";
+
+                Console.WriteLine(CalcAverage(true, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+                Console.WriteLine(CalcAverage(true, 1, 2));
+
+                int one = 0;
+                int two;
+
+                // method overloading and passing variables by value or by reference
+                SetOne(one);
+                SetOne(ref one);
+
+                SetTwo(out two);
+
+                // the TryParse() method!
+                string sNumber = null;
+                int nNumber;
+                do
+                {
+                    Console.Write("Enter a number: ");
+                    sNumber = Console.ReadLine();
+                } while (!int.TryParse(sNumber, out nNumber));
+            }
+
+
+            ///////////////////////////////////////////////////////
+            // Structs
+            ///////////////////////////////////////////////////////
+            {
+                StudentStruct[] students = new StudentStruct[2];
+
+                students[0].eGender = GenderPronoun.her;
+                students[0].eCollegeYear = CollegeYear.freshman;
+                students[0].sName = "June Smith";
+                students[0].grade = 56.9;
+                students[0].Password = "pass1234";
+
+                students[1].eGender = GenderPronoun.them;
+                students[1].eCollegeYear = CollegeYear.junior;
+                students[1].sName = "Jan Smith";
+                students[1].grade = 99.9;
+
+                foreach (StudentStruct student in students)
+                {
+                    Console.WriteLine($"{student.sName} | {student.eCollegeYear} | {student.eGender} | {student.grade}");
+                }
+            }
+        }
+
+        // Method: Main
+        // Purpose: return whether an int is even
+        // Restrictions: None
+        static bool IsEven(int nTest)
+        {
+            // returns whether nTest is odd or even
+            bool returnVal;
+
+            // we could execute multiple returns, 
+            // but having 1 return value and 1 return statement at the end is much clearer
+
+            if (nTest % 2 == 0)
+            {
+                returnVal = true;
+                //return (true);
+            }
+            else
+            {
+                returnVal = false;
+                //return (false);
+            }
+
+            return (returnVal);
+        }
+
+        // Method: Main
+        // Purpose: Calculate the average of an array of ints.  If skew is true, add 1 to each array el
+        // Restrictions: None
+        static double CalcAverage(bool skew, params int[] intArray)
+        {
+            // calculates the average of a list of ints
+            // if skew is true, add 1 to each number
+            double returnVal = 0;
+
+            foreach (int thisInt in intArray)
+            {
+                returnVal += (thisInt + (skew ? 1 : 0));
+            }
+
+            returnVal /= intArray.Length;
+
+            return (returnVal);
+        }
+
+        // Method: Main
+        // Purpose: Set an int to 1 by value
+        // Restrictions: None
+        static void SetOne(int val)
+        {
+            val = 1;
+        }
+
+        // Method: Main
+        // Purpose: Set an int to 1 by reference
+        // Restrictions: None
+        static void SetOne(ref int val)
+        {
+            val = 1;
+        }
+
+        // Method: Main
+        // Purpose: Set an int to 1 by output reference
+        // Restrictions: None
+        static void SetTwo(out int val)
+        {
+            val = 2;
+        }
+    }
+}
+
---
+++ Week 03/Session 1/formula_arrays/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formula_arrays
+{
+    public struct ZFunction
+    {
+        public double dX;
+        public double dY;
+        public double dZ;
+
+        // the constructor for this structure
+        public ZFunction(double dX, double dY)
+        {
+            this.dX = dX;
+            this.dY = dY;
+            this.dZ = 2 * Math.Pow(dX, 3) + 3 * Math.Pow(dY, 3) + 6;
+            this.dZ = Math.Round(this.dZ, 3);
+        }
+    }
+
+    // Class: Examples
+    // Author: David Schuh
+    // Purpose: Contains examples for week #3
+    // Restrictions: Only contains code snippets.  
+    class Examples
+    {
+        // Method: Main
+        // Purpose: Calculate a 3-d formula using rectangle array, jagged array and array of struct
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            {
+                // 3-d formula rectangular array example
+
+                // implement the code to calculate: z = 2x ^ 3 + 3y ^ 3 + 6
+                // for -4 <= x <= 4 in 0.1 increments: there are 81 values of x
+                // for -2 <= y <= 5 in 0.2 increments: there are 36 values of y
+
+                // we need doubles to store all values of x, y, and z
+                double x = 0;
+                double y = 0;
+                double z = 0;
+
+                // we need ints to access the array dimensions for x and y
+                int nX = 0;
+                int nY = 0;
+
+                // we declare our 3 dimensional array to hold:
+                //        81 values of x
+                //        36 values of y for each value of x
+                //        3 values for each data point: the x, y and z
+                double[,,] zFunc = new double[81, 36, 3];
+
+                // loop through each value of x, increment the int nX after each loop
+                for (x = -4; x <= 4; x += 0.1, ++nX)
+                {
+                    // round x to 1 decimal point because of rounding issues with double
+                    x = Math.Round(x, 1);
+
+                    // start with the 0'th "y" bucket for this value of x
+                    nY = 0;
+
+                    // loop through each value of y, increment nY after each loop
+                    for (y = -2; y <= 5; y += 0.2, ++nY)
+                    {
+                        // round y to 1 decimal point because of rounding issues with double
+                        y = Math.Round(y, 1);
+
+                        // calculate z
+                        z = 2 * Math.Pow(x, 3) + 3 * Math.Pow(y, 3) + 6;
+
+                        // round z to 3 decimal places
+                        z = Math.Round(z, 3);
+
+                        // store x, y and z for this (x,y) data point
+                        zFunc[nX, nY, 0] = x;
+                        zFunc[nX, nY, 1] = y;
+                        zFunc[nX, nY, 2] = z;
+                    }
+                }
+            }
+
+            {
+                // 3-d formula jagged array example
+
+                // implement the code to calculate: z = 2x ^ 3 + 3y ^ 3 + 6
+                // for -4 <= x <= 4 in 0.1 increments: there are 81 values of x
+                // for -2 <= y <= 5 in 0.2 increments: there are 36 values of y
+
+                // we need doubles to store all values of x, y, and z
+                double x = 0;
+                double y = 0;
+                double z = 0;
+
+                // we need ints to access the array dimensions for x and y
+                int nX = 0;
+                int nY = 0;
+
+                // we declare our 3 dimensional array to hold:
+                //        81 values of x
+                //        36 values of y for each value of x
+                //        3 values for each data point: the x, y and z
+                // jagged arrays can only be declared 1 dimension at a time
+                // declare the array of 81 two dimensional arrays
+                double[][][] zFunc = new double[81][][];
+
+                for(nX = 0; nX < 81; ++nX )
+                {
+                    // declare the 36 one dimensional arrays for each value of x
+                    zFunc[nX] = new double[36][];
+
+                    for (nY = 0; nY < 36; ++nY)
+                    {
+                        // declare the single array of 3 values for each (x,y)
+                        zFunc[nX][nY] = new double[3];
+                    }
+                }
+
+                // loop through each value of x, increment the int nX after each loop
+                for (x = -4; x <= 4; x += 0.1, ++nX)
+                {
+                    // round x to 1 decimal point because of rounding issues with double
+                    x = Math.Round(x, 1);
+
+                    // start with the 0'th "y" bucket for this value of x
+                    nY = 0;
+
+                    // loop through each value of y, increment nY after each loop
+                    for (y = -2; y <= 5; y += 0.2, ++nY)
+                    {
+                        // round y to 1 decimal point because of rounding issues with double
+                        y = Math.Round(y, 1);
+
+                        // calculate z
+                        z = 2 * Math.Pow(x, 3) + 3 * Math.Pow(y, 3) + 6;
+
+                        // round z to 3 decimal places
+                        z = Math.Round(z, 3);
+
+                        // store x, y and z for this (x,y) data point
+                        zFunc[nX][nY][0] = x;
+                        zFunc[nX][nY][1] = y;
+                        zFunc[nX][nY][2] = z;
+                    }
+                }
+            }
+
+            {
+                // 3-d formula structure example
+
+                // implement the code to calculate: z = 2x ^ 3 + 3y ^ 3 + 6
+                // for -4 <= x <= 4 in 0.1 increments: there are 81 values of x
+                // for -2 <= y <= 5 in 0.2 increments: there are 36 values of y
+
+                // we need doubles to store all values of x, y, and z
+                double x = 0;
+                double y = 0;
+                double z = 0;
+
+                // we need ints to access the array dimensions for x and y
+                int nX = 0;
+                int nY = 0;
+
+
+                ZFunction[] zArray = new ZFunction[81 * 36];
+
+                int dataPointCntr = 0;
+
+                // loop through each value of x, increment the int nX after each loop
+                for (x = -4; x <= 4; x += 0.1, ++nX)
+                {
+                    // round x to 1 decimal point because of rounding issues with double
+                    x = Math.Round(x, 1);
+
+                    // start with the 0'th "y" bucket for this value of x
+                    nY = 0;
+
+                    // loop through each value of y, increment nY after each loop
+                    for (y = -2; y <= 5; y += 0.2, ++nY)
+                    {
+                        // round y to 1 decimal point because of rounding issues with double
+                        y = Math.Round(y, 1);
+
+                        // create a new structure using the contructor to calculate x,y,z and round z to 3 decimals
+                        ZFunction thisDataPoint = new ZFunction(x, y);
+                        zArray[dataPointCntr++] = thisDataPoint;
+
+                        // or we simply could have written
+                        zArray[dataPointCntr++] = new ZFunction(x, y);
+                    }
+                }
+            }
+        }
+    }
+}
