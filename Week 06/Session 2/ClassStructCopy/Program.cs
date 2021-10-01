5cedee75fa6dd8ba9ce911741c74f8f05a04f370
dxsigm <54343067+dxsigm@users.noreply.github.com>
2021-09-30 23:16:38 -0400

    Session 6-2

---
+++ Week 06/Session 2/ClassStructCopy/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassCopy
+{
+    class MyContent : ICloneable
+    {
+        public string myString;
+
+        public Object Clone()
+        {
+            // this will create a new MyContent object and copy myString
+            // from this into the new object
+            // and return the new object as System.Object
+            // MemberwiseClone() does a shallow copy of a class object
+            return this.MemberwiseClone();
+        }
+
+    }
+
+    // inherit from the ICloneable interface to support deep copy
+    class MyClass : ICloneable
+    {
+        // myContainingClass is a reference data type
+        public MyContent myContainingClass = new MyContent();
+
+        // val is a value data type
+        public int val;
+
+        // intArray is a reference data type
+        public List<int> intArray = new List<int>();
+
+        // the Clone() method is required by the interface
+        // and should implement the deep copy of this class
+        public Object Clone()
+        {
+            // first do a MemberwiseClone() of the top level of the class
+            // this is a shallow copy which creates a new object and only copies the val field
+            // myContainingClass and intArray will be copied by reference
+            MyClass copy = (MyClass)this.MemberwiseClone();
+
+            // call the MemberwiseClone() of the child class
+            // by calling its Clone() method
+            // note that MemberwiseClone is "protected" therefore it can only
+            // be called within its own class
+            // ie. we cannot do:
+            // copy.myContainingClass = (MyContent)this.myContainingClass.MemberwiseClone();
+            // MemberwiseClone() and Clone() return System.Object, therefore we need
+            // to explicitly cast the object to the correct type
+            copy.myContainingClass = (MyContent)this.myContainingClass.Clone();
+
+            // GetRange() creates a new shoebox copy of a List<>
+            copy.intArray = this.intArray.GetRange(0, intArray.Count);
+
+            // return copy which is a MyClass object as System.Object
+            // note that object = copy is valid (any class type can be referenced by a parent class such as object)
+            // but copy = object is invalid (any object cannot be any class type)
+            // for example, a Ming vase can be easily turned into an object (if you drop it for example)
+            //       object = Ming vase  (valid)
+            // but any object cannot be a Ming vase!
+            //       Ming vase = object (invalid)
+            return copy;
+        }
+    }
+
+    // structures equivalent to the classes
+    struct MyContentStruct
+    {
+        public string myString;
+    }
+
+    struct MyStruct
+    {
+        public MyContentStruct myContentStruct;
+        public int val;
+        public List<int> intArray;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass objectA = new MyClass();
+            objectA.myContainingClass.myString = "david";
+
+            // if we want to copy objectA into objectB
+            MyClass objectB;
+
+            // this is NOT the way to do it!
+            // classes are reference data types
+            // both variables are pointing to the same object
+            // there is only 1 shoebox which was created by objectA
+            objectB = objectA;
+
+            // one way is to create the new object
+            objectB = new MyClass();
+
+            // and explicitly copy every class member
+            objectB.val = objectA.val;
+
+            // and copy each member of the containing class
+            objectB.myContainingClass.myString = objectA.myContainingClass.myString;
+
+            // deep copy uses the ICloneable interface
+            // and depends on our creating a Clone() method in the class
+            // that does the field-by-field member copy
+            // create an interface variable
+            ICloneable cloneable;
+
+            // set the interface variable equal to the object to copy
+            cloneable = objectA;
+
+            // call the Clone() method and return the correct class type
+            objectB = (MyClass)cloneable.Clone();
+
+            // we could also just call Clone() via objectA
+            objectB = (MyClass)objectA.Clone();
+
+
+            // compare the same code with structs
+            // structs are value data types
+            MyStruct structA;
+            structA.myContentStruct.myString = "david";
+            structA.val = 40;
+            structA.intArray = new List<int>();
+            structA.intArray.Add(1);
+            structA.intArray.Add(2);
+            structA.intArray.Add(3);
+            structA.intArray.Add(4);
+            structA.intArray.Add(5);
+
+            // we now have 2 separate structures
+            MyStruct structB = structA;
+
+            // this does not change structA
+            structB.myContentStruct.myString = "sue;"
+
+            // however this does, because List<> is a reference data type
+            structB.intArray.Add(6);
+
+            // we can create a separate intArray in structB by using the GetRange() method
+            structB.intArray = structA.intArray.GetRange(0, structA.intArray.Count);
+
+            // and now 7 will only be added to structB's intArray
+            structB.intArray.Add(7);
+
+            // if "new" is required to create the variable, then you should assume it is 
+            // a reference variable.  Note that "new" is not required to create struct variables
+        }
+    }
+}
---
+++ Week 06/Session 2/GameOfLife/Program.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace GameOfLife
+{
+    public enum EAliveState
+    {
+        alive,
+        dead
+    }
+
+    public enum EDirection
+    {
+        right,
+        down,
+        left,
+        up,
+        topLeft,
+        topRight,
+        bottomLeft,
+        bottomRight
+    }
+
+    public struct StructCellState
+    {
+        public EAliveState eAliveState;
+    }
+
+    static public class Game
+    {
+        public static bool bExit = false;
+        public static Random rand = new Random();
+
+        public static int MAX_ROWS = 30;
+        public static int MAX_COLS = 80;
+
+        public static Cell[,] organism;
+
+        public class Cell
+        {
+            public static int MAX_NEIGHBORS = Enum.GetNames(typeof(EDirection)).Length;
+
+            public Cell[] neighbor; //  = new Cell[MAX_NEIGHBORS];
+
+            public Cell nextCell;
+
+            public StructCellState currentCellState;
+            public StructCellState nextCellState;
+
+            public Cell(int maxCells, int probability = 6)
+            {
+                neighbor = new Cell[MAX_NEIGHBORS];
+
+                probability = rand.Next(0, probability);
+
+                if( probability == 0)
+                {
+                    currentCellState.eAliveState = EAliveState.alive;
+                }
+                else
+                {
+                    currentCellState.eAliveState = EAliveState.dead;
+                }
+            }
+
+            public void SetNextState()
+            {
+                int nAlive = 0;
+
+                for(int nCntr = 0; nCntr < MAX_NEIGHBORS; ++nCntr)
+                {
+                    Cell neighborCell = neighbor[nCntr];
+                    if( neighborCell != null)
+                    {
+                        if( neighborCell.currentCellState.eAliveState == EAliveState.alive)
+                        {
+                            ++nAlive;
+                        }
+                    }
+                }
+
+                nextCellState = currentCellState;
+
+                if( nAlive < 2 || nAlive > 3)
+                {
+                    nextCellState.eAliveState = EAliveState.dead;
+                }
+                else
+                {
+                    if( nAlive == 3)
+                    {
+                        nextCellState.eAliveState = EAliveState.alive;
+                    }
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            organism = new Cell[MAX_ROWS, MAX_COLS];
+            for( int row = 0; row < MAX_ROWS; ++row)
+            {
+                for(int col = 0; col < MAX_COLS; ++col)
+                {
+                    organism[row, col] = new Cell(MAX_ROWS * MAX_COLS);
+                }
+            }
+
+            for (int row = 0; row < MAX_ROWS; ++row)
+            {
+                for (int col = 0; col < MAX_COLS; ++col)
+                {
+                    Cell thisCell = organism[row, col];
+                    Cell nextCell = null;
+
+                    for( int nCntr = 0; nCntr < Cell.MAX_NEIGHBORS; ++nCntr)
+                    {
+                        Cell neighborCell = null;
+
+                        switch(nCntr)
+                        {
+                            case (int)EDirection.right:
+                                if(col < MAX_COLS - 1)
+                                {
+                                    neighborCell = organism[row, col + 1];
+                                    nextCell = organism[row, col + 1];
+                                }
+                                else if(row < MAX_ROWS - 1)
+                                {
+                                    nextCell = organism[row + 1, 0];
+                                }
+                                break;
+
+                            case (int)EDirection.down:
+                                // bottom neighbor = (row + 1, col)
+                                if (row < MAX_ROWS - 1)
+                                {
+                                    neighborCell = organism[row + 1, col];
+                                }
+                                break;
+
+                            case (int)EDirection.left:
+                                // left neighbor = (row, col - 1)
+                                if (col > 0)
+                                {
+                                    neighborCell = organism[row, col - 1];
+                                }
+                                break;
+
+                            case (int)EDirection.up:
+                                // top neighbor = (row - 1, col)
+                                if (row > 0)
+                                {
+                                    neighborCell = organism[row - 1, col];
+                                }
+                                break;
+
+                            case (int)EDirection.topRight:
+                                // topright neighbor = (row - 1, col + 1)
+                                if (row > 0 && col < MAX_COLS - 1)
+                                {
+                                    neighborCell = organism[row - 1, col + 1];
+                                }
+
+                                break;
+
+                            case (int)EDirection.bottomRight:
+                                // bottomright neighbor = (row + 1, col + 1)
+                                if (row < MAX_ROWS - 1 && col < MAX_COLS - 1)
+                                {
+                                    neighborCell = organism[row + 1, col + 1];
+                                }
+                                break;
+
+                            case (int)EDirection.topLeft:
+                                // topleft neighbor = (row - 1, col - 1)
+                                if (col > 0 && row > 0)
+                                {
+                                    neighborCell = organism[row - 1, col - 1];
+                                }
+                                break;
+
+                            case (int)EDirection.bottomLeft:
+                                // bottomleft neighbor = (row + 1, col - 1)
+                                if (row < MAX_ROWS - 1 && col > 0)
+                                {
+                                    neighborCell = organism[row + 1, col - 1];
+                                }
+                                break;
+                        }
+
+                        thisCell.neighbor[nCntr] = neighborCell;
+                    }
+
+                    thisCell.nextCell = nextCell;                    
+                }
+            }
+
+            // since we will use Unicode characters, set the console to display Unicode
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            // assign a delegate method to handle when CTRL+C is pressed
+            // in order to exit our infinite game loop
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(ConsoleCancel);
+
+            while(!bExit)
+            {
+                // print the current state of the organism
+                PrintOrganism(organism, MAX_ROWS, MAX_COLS);
+
+                // calculate the next state of every cell based on the current state of every cell
+                CalculateNextGeneration(organism[0, 0]);
+
+                // sleep for 100 milliseconds
+                Thread.Sleep(100);
+            }
+
+        }
+
+
+        public static void ConsoleCancel(object sender, ConsoleCancelEventArgs e)
+        {
+            // CTRL+C sets bExit = true
+            bExit = true;
+        }
+
+        // recursive method to calculate the next state of every cell based on the current state of all neighbors
+        public static void CalculateNextGeneration(Cell thisCell)
+        {
+            // base case is if we reached thisCell.nextCell == null
+            if (thisCell != null)
+            {
+                // calculate the next state for the current cell
+                thisCell.SetNextState();
+
+                // recurse through the whole linked list of thisCell.nextCell (moves through all cells to the "right")
+                CalculateNextGeneration(thisCell.nextCell);
+
+                // after all next states have been calculated for the organism
+                // unfold the calling stack for every cell
+                // and set the current state = the next state 
+                // (these are structures, so they can be copied by value)
+                thisCell.currentCellState = thisCell.nextCellState;
+            }
+        }
+
+
+        public static void PrintOrganism(Cell[,] organism, int maxRows, int maxCols)
+        {
+            string output = "----------------------------------------------------------------------------------\n";
+
+            for (int row = 0; row < maxRows; ++row)
+            {
+                output += "|";
+                for (int col = 0; col < maxCols; ++col)
+                {
+                    Cell thisCell = organism[row, col];
+                    if (thisCell.currentCellState.eAliveState == EAliveState.alive)
+                    {
+                        output += "\x25cb";
+                    }
+                    else
+                    {
+                        output += " ";
+                    }
+                }
+
+                output += "|\n";
+            }
+
+            output += "----------------------------------------------------------------------------------";
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+            Console.Write(output);
+        }
+    }
+}
